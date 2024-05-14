f2c8090e2bf04c404c9295d1d38124ba3ec44f48
TomBENSC <tbalay001@ensc.fr>
2024-05-14 23:11:37 +0200

    Ajout missions

--- Characters/Object/Object.cs
+++ Characters/Object/Object.cs
@@ -1,6 +1,6 @@
 class Object : Character
 {
-    public Object(bool unlock, int inventoryWeight, int boatWeight) : base(unlock, 500, 5, inventoryWeight, boatWeight, "Non", "Montagne/Eau", 5, "Zigzag")
+    public Object(bool unlock, int inventoryWeight, int boatWeight) : base(unlock, 500, 5, inventoryWeight, boatWeight, "Non", "Montagne/Eau", 5, "Diagonale")
     {}
 
     public override void DisplayCharacter(Character character)
